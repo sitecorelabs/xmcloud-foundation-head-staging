23c674593a67bc65a3866c89e08d7d33e1694174
illiakovalenko <illia.kovalenko@sitecore.com>
2023-08-03 12:35:23 +0300

    [JSS 21.2.3][sitecore-jss-nexjts] Redirects don't work when file extensions present in a route

--- src/sxastarter/src/middleware.ts
+++ src/sxastarter/src/middleware.ts
@@ -16,5 +16,5 @@ export const config = {
    * 5. /healthz (Health check)
    * 6. all root files inside /public (e.g. /favicon.ico)
    */
-  matcher: ['/', '/((?!api/|_next/|healthz|sitecore/api/|-/|[\\w-]+\\.\\w+).*)'],
+  matcher: ['/', '/((?!api/|_next/|healthz|sitecore/api/|-/|favicon.ico|sc_logo.svg).*)'],
 };
